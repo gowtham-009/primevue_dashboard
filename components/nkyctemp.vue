fb27b93c5b6dfa4eee4350f70fb535ff3a1243be
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-07 12:25:14 +0530

    Update nkyctemp.vue

--- components/nkyctemp.vue
+++ components/nkyctemp.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full flex justify-center" >
-       <iframe src="http://nkyc-latest-app.vercel.app" frameborder="0" class="w-1/3 h-screen"></iframe>
+       <iframe src="https://nkyc-latest-app.vercel.app" frameborder="0" class="w-1/3 h-screen"></iframe>
     </div>
 </template>
 
