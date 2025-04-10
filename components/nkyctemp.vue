0772b9a1fa0f4844e3b3ece824eaa793ce0fd510
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-10 16:55:33 +0530

    cha

--- components/nkyctemp.vue
+++ components/nkyctemp.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-full flex justify-center" >
-       <iframe src="https://nkyc-latest-app.vercel.app"  allow="camera; microphone; geolocation" frameborder="0" class="w-1/3 h-screen"></iframe>
+       <iframe src="https://nkyc-app.vercel.app"  allow="camera; microphone; geolocation" frameborder="0" class="w-1/3 h-screen"></iframe>
     </div>
 </template>
 
