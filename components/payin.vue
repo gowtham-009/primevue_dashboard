4f776be964a8f2b8250c3bf9d929d4883ed45512
gowtham-009 <139350157+gowtham-009@users.noreply.github.com>
2025-04-12 09:56:43 +0530

    ijio

--- components/payin.vue
+++ components/payin.vue
@@ -3,10 +3,10 @@
 
 <template>
     <div class="w-full h-full">
-        <iframe class="w-full h-full" :src="'https://smartpayz.gwcindia.in/smartpayz/G1-Single.php?ce=' + customCode" />
+        <iframe class="w-full h-full" :src="'https://smartpayz.gwcindia.in/smartpayz/G1-Single.php?Ce=GZ10219'" />
     </div>
   </template>
   
   <script setup>
-  const customCode = "GZ10219";
+//   const customCode = "GZ10219";
   </script>
